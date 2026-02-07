0346a80adec91038156505b98ebd8d690c694cb4
Morixx <98456451+morixxfoxdata@users.noreply.github.com>
2026-02-07 16:42:17 +0900

    Add Latex config for LazyVim

---
+++ home/dot_config/nvim/lua/plugins/latex.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "lervag/vimtex",
+    init = function()
+      vim.g.vimtex_view_method = "skim"
+      vim.g.vimtex_view_skim_sync = 1
+      vim.g.vimtex_view_skim_reading_bar = 1
+    end,
+  },
+}
