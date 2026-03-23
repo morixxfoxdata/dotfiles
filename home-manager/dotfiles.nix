2c773beb78aff1d434603773a0e7ed15d591663f
Morixx <98456451+morixxfoxdata@users.noreply.github.com>
2026-03-23 23:40:21 +0900

    feat: bring all tool configs under Home Manager management
    Add config files for nvim, starship, zsh, gh, karabiner, aerospace,
    and ghostty. Use link_force activation script to create mutable
    symlinks directly to the repo, inspired by ryoppippi/dotfiles.

---
+++ home-manager/dotfiles.nix
@@ -0,0 +1,42 @@
+{ config, lib, ... }:
+
+let
+  dotfilesDir = "${config.home.homeDirectory}/dotfiles/home-manager";
+in
+{
+  home.activation.linkDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    link_force() {
+      local src=$1
+      local dst=$2
+      $DRY_RUN_CMD mkdir -p "$(dirname "$dst")"
+      $DRY_RUN_CMD rm -rf "$dst"
+      $DRY_RUN_CMD ln -sf "$src" "$dst"
+    }
+
+    # Git
+    link_force "${dotfilesDir}/git/.gitconfig" "$HOME/.gitconfig"
+    link_force "${dotfilesDir}/git/ignore" "$HOME/.config/git/ignore"
+
+    # Neovim
+    link_force "${dotfilesDir}/nvim" "$HOME/.config/nvim"
+
+    # Starship
+    link_force "${dotfilesDir}/starship/starship.toml" "$HOME/.config/starship.toml"
+
+    # Zsh
+    link_force "${dotfilesDir}/zsh/.zshrc" "$HOME/.zshrc"
+    link_force "${dotfilesDir}/zsh/.zshenv" "$HOME/.zshenv"
+
+    # GitHub CLI
+    link_force "${dotfilesDir}/gh/config.yml" "$HOME/.config/gh/config.yml"
+
+    # Karabiner
+    link_force "${dotfilesDir}/karabiner/karabiner.json" "$HOME/.config/karabiner/karabiner.json"
+
+    # Aerospace
+    link_force "${dotfilesDir}/aerospace/aerospace.toml" "$HOME/.config/aerospace/aerospace.toml"
+
+    # Ghostty
+    link_force "${dotfilesDir}/ghostty/config" "$HOME/.config/ghostty/config"
+  '';
+}
---
+++ home-manager/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      replace_netrw = true,
+    },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+  },
+}
